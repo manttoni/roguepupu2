6b9f1387242a9ae66d29566aa9ce5f097f686098
Anttoni Maula <manttoni92@gmail.com>
2025-12-11 01:22:41 +0200

    More getters for ECS

--- headers/ECS.hpp
+++ headers/ECS.hpp
@@ -23,4 +23,10 @@ namespace ECS
 	bool has_weapon_property(const entt::registry& registry, const entt::entity entity, const std::string& property);
 	std::map<std::string, std::string> get_info(const entt::registry& registry, const entt::entity entity);
 	Cell* get_cell(const entt::registry& registry, const entt::entity entity);
+	size_t get_value(const entt::registry& registry, const entt::entity entity);
+	double distance(const entt::registry& registry, const entt::entity a, const entt::entity b);
+	entt::entity get_player(const entt::registry& registry);
+	bool is_equippable(const entt::registry& registry, const entt::entity entity);
+	std::string get_subcategory(const entt::registry& registry, const entt::entity entity);
+	bool can_see(const entt::registry& registry, const entt::entity seer, const entt::entity target);
 };
