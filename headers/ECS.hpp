92e2bce77f8d9462675a58b2db3bc83e38ce1420
Anttoni Maula <manttoni92@gmail.com>
2025-12-10 11:21:02 +0200

    Added getters to ECS to get specific info about entities. Cell uses these to get color and glyph of entities, which don't always have defined them explicitly.

--- headers/ECS.hpp
+++ headers/ECS.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
 #include <string>
+#include <map>
 #include "entt.hpp"
 #include "Color.hpp"
+#include "Components.hpp"
+
+#define MELEE_RANGE 1.5
 
 namespace ECS
 {
+	std::string get_category(const entt::registry& registry, const entt::entity entity);
 	Color get_rarity_color(const std::string& rarity);
 	Color get_color(const entt::registry& registry, const entt::entity entity);
+	wchar_t get_glyph(const entt::registry& registry, const entt::entity entity);
+	std::string get_name(const entt::registry& registry, const entt::entity entity);
 	std::string get_colored_name(const entt::registry& registry, const entt::entity entity);
-	std::string get_description(const entt::registry& registry, const entt::entity entity);
+	std::string get_proficiency_requirement(const entt::registry& registry, const entt::entity entity);
+	size_t get_armor_class(const entt::registry& registry, const entt::entity entity);
+	Damage get_damage(const entt::registry& registry, const entt::entity entity);
+	Dice get_versatile_dice(const entt::registry& registry, const entt::entity entity);
+	bool has_weapon_property(const entt::registry& registry, const entt::entity entity, const std::string& property);
+	std::map<std::string, std::string> get_info(const entt::registry& registry, const entt::entity entity);
+	Cell* get_cell(const entt::registry& registry, const entt::entity entity);
 };
