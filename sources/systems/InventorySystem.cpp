3877ca818e80e5d5f7d11c5355ff9eb648dc3b86
Anttoni Maula <manttoni92@gmail.com>
2025-12-10 11:39:07 +0200

    Dropping items from inventory to ground

--- sources/systems/InventorySystem.cpp
+++ sources/systems/InventorySystem.cpp
@@ -1,10 +1,13 @@
-#include <string>                       // for basic_string, operator+, char...
-#include "Components.hpp"               // for Inventory, Name, Player (ptr ...
-#include "ECS.hpp"                      // for get_name, get_description
+#include <string>                       // for basic_string, char_traits
+#include "Cave.hpp"                     // for Cave
+#include "Cell.hpp"                     // for Cell
+#include "Components.hpp"               // for Position, Inventory, Name
+#include "ECS.hpp"                      // for get_cell, get_colored_name
 #include "UI.hpp"                       // for UI
-#include "Utils.hpp"                    // for error, capitalize, top_left
+#include "Utils.hpp"                    // for topleft, capitalize
 #include "entt.hpp"                     // for vector, allocator, basic_sigh...
-#include "systems/EquipmentSystem.hpp"  // for equip_or_unequip, is_equipped
+#include "systems/EquipmentSystem.hpp"  // for is_equipped, equip_or_unequip
+#include "systems/ExamineSystem.hpp"    // for get_info_neat
 #include "systems/InventorySystem.hpp"  // for add_item, has_item, inventory...
 
 namespace InventorySystem
@@ -14,7 +17,7 @@ namespace InventorySystem
 		return key == 'i';
 	}
 
-	std::vector<std::string> get_item_names(const entt::registry& registry, const std::vector<entt::entity>& items)
+	std::vector<std::string> get_colored_item_names(const entt::registry& registry, const std::vector<entt::entity>& items)
 	{
 		auto player = *registry.view<Player>().begin();
 		std::vector<std::string> names;
@@ -36,6 +39,8 @@ namespace InventorySystem
 
 	void remove_item(entt::registry& registry, const entt::entity entity, const entt::entity item)
 	{
+		if (EquipmentSystem::is_equipped(registry, entity, item))
+			EquipmentSystem::unequip(registry, entity, item);
 		auto& inventory = registry.get<Inventory>(entity).inventory;
 		auto it = std::find(inventory.begin(), inventory.end(), item);
 		inventory.erase(it);
@@ -53,56 +58,16 @@ namespace InventorySystem
 		add_item(registry, looter, item);
 	}
 
-	std::string get_proficiency(const entt::registry& registry, const entt::entity item)
+	void drop_item(entt::registry& registry, const entt::entity owner, const entt::entity item)
 	{
-		std::string proficiency = "";
-		if (registry.all_of<Weapon>(item))
-			proficiency = registry.get<Weapon>(item).proficiency + " weapons";
-		if (registry.all_of<Armor>(item))
-			proficiency = registry.get<Armor>(item).proficiency + " armor";
-		return Utils::capitalize(proficiency);
-	}
-
-	std::string get_damage(const entt::registry& registry, const entt::entity item)
-	{
-		std::string damage = "", versatile_damage = "";
-		if (registry.all_of<Damage>(item))
+		remove_item(registry, owner, item);
+		Cell* cell = ECS::get_cell(registry, owner);
+		registry.emplace<Position>(item, cell);
+		if (registry.all_of<Glow>(item))
 		{
-			const auto& dmg_cmp = registry.get<Damage>(item);
-			damage += dmg_cmp.dice.get_string() + " ";
-			if (registry.all_of<Weapon>(item))
-				versatile_damage = registry.get<Weapon>(item).versatile_dice.get_string();
-			if (!versatile_damage.empty())
-				damage += "( " + versatile_damage + " ) ";
-			damage += dmg_cmp.type;
+			cell->get_cave()->reset_lights();
+			cell->get_cave()->draw();
 		}
-		return Utils::capitalize(damage);
-	}
-
-	std::string get_armor_class(const entt::registry& registry, const entt::entity item)
-	{
-		if (registry.all_of<Armor>(item))
-			return std::to_string(registry.get<Armor>(item).armor_class);
-		return "";
-	}
-
-	std::vector<std::string> get_item_stats(const entt::registry& registry, const entt::entity item)
-	{
-		std::vector<std::string> stats = {"*** " + ECS::get_colored_name(registry, item) + " ***"};
-
-		std::string proficiency = get_proficiency(registry, item);
-		if (!proficiency.empty())
-			stats.push_back("Proficiency: " + proficiency);
-
-		std::string damage = get_damage(registry, item);
-		if (!damage.empty())
-			stats.push_back("Damage: " + damage);
-
-		std::string armor_class = get_armor_class(registry, item);
-		if (!armor_class.empty())
-			stats.push_back("AC: " + armor_class);
-
-		return stats;
 	}
 
 	std::vector<std::string> get_options(const entt::registry& registry, const entt::entity owner, const entt::entity item)
@@ -120,6 +85,7 @@ namespace InventorySystem
 			}
 		}
 		else options.push_back("Loot");
+		options.push_back("Drop");
 		options.push_back("Cancel");
 		return options;
 	}
@@ -137,10 +103,10 @@ namespace InventorySystem
 		{
 			// Create list of items to show in inventory window
 			auto& items = registry.get<Inventory>(owner).inventory;
-			const auto& names = get_item_names(registry, items);
+			const auto& names = get_colored_item_names(registry, items);
 
 			// Dialog returns selection
-			selection = UI::instance().dialog("*** " + Utils::capitalize(owner_name) + " ***", names, Screen::top_left(), idx + 1);
+			selection = UI::instance().dialog("*** " + Utils::capitalize(owner_name) + " ***", names, Screen::topleft(), idx + 1);
 			if (selection.empty()) // Esc was pressed
 				break;
 
@@ -149,14 +115,15 @@ namespace InventorySystem
 			idx = it - names.begin();
 			auto& item = items[idx];
 
-			// Show stats and options
-			const auto& stats = get_item_stats(registry, item);
 			const auto& options = get_options(registry, owner, item);
-			selection = UI::instance().dialog(stats, options, Screen::top_left());
+			const auto& info_neat = ExamineSystem::get_info_neat(registry, item);
+			selection = UI::instance().dialog(info_neat, options, Screen::topleft());
 			if (selection == "Equip" || selection == "Unequip")
 				EquipmentSystem::equip_or_unequip(registry, player, item);
 			else if (selection == "Loot")
 				loot_item(registry, player, owner, item);
+			else if (selection == "Drop")
+				drop_item(registry, owner, item);
 		}
 	}
 };
