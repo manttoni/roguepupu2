8c63c78014db4d2fc2c150e052ed6e66aee493c5
Anttoni Maula <manttoni92@gmail.com>
2026-01-02 06:46:14 +0200

    Devtools data

--- sources/DevTools.cpp
+++ sources/DevTools.cpp
@@ -6,14 +6,32 @@
 #include "ECS.hpp"
 #include "Cave.hpp"
 #include "Cell.hpp"
+#include "Components.hpp"
 
 namespace DevTools
 {
+
+	std::string get_amount(const entt::registry& registry, const std::string& category)
+	{
+		auto all = registry.view<Category>();
+		std::vector<entt::entity> matches;
+		for (const auto e : all)
+		{
+			if (registry.get<Category>(e).category == category ||
+					registry.get<Subcategory>(e).subcategory == category)
+				matches.push_back(e);
+		}
+		return std::to_string(matches.size()) + " " + category;
+	}
+
 	void dev_menu(entt::registry& registry)
 	{
 		const std::vector<std::string> data =
 		{
-			"Show stuff here"
+			"Registry has...",
+			get_amount(registry, "creatures"),
+			get_amount(registry, "mushrooms"),
+			get_amount(registry, "plants")
 		};
 		const std::vector<std::string> choices =
 		{
